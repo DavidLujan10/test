657c6eeb19229f12b2b34ef86670306e0170332e
DavidLujan10 <JoseDavid.LujanGallegos@hotmail.com>
2017-09-07 10:44:59 -0500

    Desplazamiento

--- bonsai.js/www/js/app.js
+++ bonsai.js/www/js/app.js
@@ -1,3 +1,12 @@
 new Rect(10, 10, 100, 100)
 .addTo(stage)
 .attr('fillColor', 'green')
+.animate('5s', {
+    x: 800,//HASTA DONDE SE DESPLAZA LA FIGURA 
+    //Y: 900
+  }, { 
+    //easing: 'bounceIn',//PARA QUE REGRESE Y HAGA UN TIPO BRINCO
+    delay: '200ms'
+  })
+  /*.repeat(dos)
+  return dos;*/
