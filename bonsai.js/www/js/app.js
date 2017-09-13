3fada797745efb0ce299f412ff74887472d4d56d
DavidLujan10 <JoseDavid.LujanGallegos@hotmail.com>
2017-09-12 21:06:51 -0500

    tarea

--- bonsai.js/www/js/app.js
+++ bonsai.js/www/js/app.js
@@ -22,22 +22,40 @@
   new Circle(0, 5, 6)
   .addTo(stage)
   .attr('fillColor', 'yellow')
-  .animate('5s', {
-    x: 800,//HASTA DONDE SE DESPLAZA LA FIGURA 
-    Y: 700
+  .animate('10s', {
+    x: 0,
+    Y: 0
   }, { 
-    //easing: 'bounceIn',//PARA QUE REGRESE Y HAGA UN TIPO BRINCO
-    delay: '200ms'
-  });
+
+    delay: '10s'
+  })
+  .animate('5s', {
+    y: 450
+  },{
+    delay:'5s'
+  })
+  .animate('5s',{
+    x: 300
+  })
+
   
-  new Arc(0, 5, 10, 45, 45, false)
+  new Arc(10, 10, 6, 0, 9, 9)
   .addTo(stage)
   .attr('fillColor', 'red')
-  .animate('5s', {
-    x: 800,//HASTA DONDE SE DESPLAZA LA FIGURA 
-    Y: 700
+  .animate('11s', {
+    x: 0,
+    Y: 0
   }, { 
-    //easing: 'bounceIn',//PARA QUE REGRESE Y HAGA UN TIPO BRINCO
-    delay: '200ms'
+    
+    delay: '11s'
+  })
+  .animate('6s',{
+    y: 450
+  },{
+    delay: '6s'
+  })
+  .animate('6s',{
+    x:300
   })
   
+ 
