1a74ccca13a6c01dc279d4c2329039c21aae3aac
DavidLujan10 <JoseDavid.LujanGallegos@hotmail.com>
2017-09-18 21:01:55 -0500

    Project

--- Project/www/js/movie.js
+++ Project/www/js/movie.js
@@ -1,4 +1,26 @@
-//CON EL ACENTO GA
+//CON EL ACENTO GRAVE MAS EL $ Y {} INTERPOLAMOS VARIABLES
+
+var rectPath = new Rect(0,0,150,150)
+.attr({
+    fillColor: 'pink',
+    strokeColor: 'green',//ES EL COLOR DEL BORDER Y ESTO ES CUANDO ESTA DIBUJANDO UNA LINEA AL REDEDOR DE LA FIGURA
+    strokeWidth: 5,
+    x: 550,
+    y: 650
+});
+
+stage.addChild(rectPath);
+
+rectPath.animate('2s', {
+    rotation: Math.PI*2,
+    //x: 800,
+    /*fillColor: 'blue',
+    fillColor: 'pink',
+    fillColor: 'black'*/
+},{
+    easing: 'elasticInOut' //TIPO DE ANIMACION QUE PODEMOS USAR
+})
+
 
 var rectPath = new Rect(0,0,150,150)
 .attr({
@@ -12,40 +34,44 @@ var rectPath = new Rect(0,0,150,150)
 stage.addChild(rectPath);
 
 rectPath.animate('2s', {
-    rotation: Math.PI*2,
-  x: 800,
-   fillColor: 'blue',
+    //rotation: Math.PI*2,
+    x: 800,
+    /*fillColor: 'blue',
     fillColor: 'pink',
-   fillColor: 'black',
+    fillColor: 'black'*/
 },{
     easing: 'elasticInOut' //TIPO DE ANIMACION QUE PODEMOS USAR
 })
-var rectPath = new Rect(50,50,150,150)
+
+
+var rectPath = new Rect(0,0,150,150)
 .attr({
-    fillColor: 'red',
+    fillColor: 'blue',
     strokeColor: 'green',//ES EL COLOR DEL BORDER Y ESTO ES CUANDO ESTA DIBUJANDO UNA LINEA AL REDEDOR DE LA FIGURA
     strokeWidth: 5,
     x: 800,
-    y: 300
+    y: 450
 });
 
 stage.addChild(rectPath);
 
 rectPath.animate('2s', {
-    rotation: Math.PI*2,
-  x: 150,
-  fillColor: 'blue',
-  fillColor: 'green',
-  fillColor: 'blue',
-  fillColor: 'pink',
+    //rotation: Math.PI*2,
+    x: 150,
+    /*fillColor: 'blue',
+    fillColor: 'green',
+    fillColor: 'blue',
+    fillColor: 'pink',*/
  
 
 },{
     easing: 'elasticInOut' //TIPO DE ANIMACION QUE PODEMOS USAR
 })
-var rectPath = new Rect(50,50,150,150)
+
+
+var rectPath = new Rect(0,0,150,150)
 .attr({
-    fillColor: 'red',
+    fillColor: 'yellow',
     strokeColor: 'green',//ES EL COLOR DEL BORDER Y ESTO ES CUANDO ESTA DIBUJANDO UNA LINEA AL REDEDOR DE LA FIGURA
     strokeWidth: 5,
     x: 150,
@@ -55,10 +81,10 @@ var rectPath = new Rect(50,50,150,150)
 stage.addChild(rectPath);
 
 rectPath.animate('2s', {
-    rotation: Math.PI*2,
-  x: 800,
-  y:300,
-  fillColor: 'yellow'
+    //rotation: Math.PI*2,
+    x: 800,
+    y:450,
+    //fillColor: 'yellow'
 },{
     easing: 'elasticInOut' //TIPO DE ANIMACION QUE PODEMOS USAR
 })
