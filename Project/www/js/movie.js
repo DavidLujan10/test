0ffcae9dc8d4f7659f5308cf0388de151dda1c5c
DavidLujan10 <JoseDavid.LujanGallegos@hotmail.com>
2017-09-18 20:50:20 -0500

    Project

--- Project/www/js/movie.js
+++ Project/www/js/movie.js
@@ -12,7 +12,53 @@ var rectPath = new Rect(0,0,150,150)
 stage.addChild(rectPath);
 
 rectPath.animate('2s', {
-    rotation: Math.PI*2
+    rotation: Math.PI*2,
+  x: 800,
+   fillColor: 'blue',
+    fillColor: 'pink',
+   fillColor: 'black',
+},{
+    easing: 'elasticInOut' //TIPO DE ANIMACION QUE PODEMOS USAR
+})
+var rectPath = new Rect(50,50,150,150)
+.attr({
+    fillColor: 'red',
+    strokeColor: 'green',//ES EL COLOR DEL BORDER Y ESTO ES CUANDO ESTA DIBUJANDO UNA LINEA AL REDEDOR DE LA FIGURA
+    strokeWidth: 5,
+    x: 800,
+    y: 300
+});
+
+stage.addChild(rectPath);
+
+rectPath.animate('2s', {
+    rotation: Math.PI*2,
+  x: 150,
+  fillColor: 'blue',
+  fillColor: 'green',
+  fillColor: 'blue',
+  fillColor: 'pink',
+ 
+
+},{
+    easing: 'elasticInOut' //TIPO DE ANIMACION QUE PODEMOS USAR
+})
+var rectPath = new Rect(50,50,150,150)
+.attr({
+    fillColor: 'red',
+    strokeColor: 'green',//ES EL COLOR DEL BORDER Y ESTO ES CUANDO ESTA DIBUJANDO UNA LINEA AL REDEDOR DE LA FIGURA
+    strokeWidth: 5,
+    x: 150,
+    y: 150
+});
+
+stage.addChild(rectPath);
+
+rectPath.animate('2s', {
+    rotation: Math.PI*2,
+  x: 800,
+  y:300,
+  fillColor: 'yellow'
 },{
     easing: 'elasticInOut' //TIPO DE ANIMACION QUE PODEMOS USAR
 })
